1311464eaffa6fcb2c351f997c3a23ae01463b60
Ahmed Sabrah <support@clickpay.sa>
2023-03-15 15:50:01 +0300

    first commit

---
+++ Clickpay-Sample/Data/Clickpay_SampleContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Clickpay_Sample.Models;
+
+namespace Clickpay_Sample.Data
+{
+    public class Clickpay_SampleContext : DbContext
+    {
+        public Clickpay_SampleContext(DbContextOptions<Clickpay_SampleContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Transaction> Transaction { get; set; }
+    }
+}
